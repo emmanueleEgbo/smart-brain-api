23127e18600597f7a94425e354cd30b9dee272f7
Emmsecur3 <emmsecur3@gmail.com>
2023-06-24 13:26:22 +0100

    fixing database connection

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => {image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
   
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`app is running on port ${port} !!!!!`);
